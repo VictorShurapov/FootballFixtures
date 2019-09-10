5a7def08772d086831c80109f96fba90322fab74
Viktor S <vshurapov@gmail.com>
2019-09-10 16:31:45 +0300

    Initial commit

---
+++ ForzaFixtures1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ForzaFixtures1
+//
+//  Created by ViktorX on 9/10/19.
+//  Copyright © 2019 ViktorX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
