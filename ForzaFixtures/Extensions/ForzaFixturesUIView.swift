e684a82cb5c998a4a4b0208550fcffff649dedd3
Viktor S <vshurapov@gmail.com>
2019-10-03 18:38:48 +0300

    ForzaFixturesUIView extension

---
+++ ForzaFixtures/Extensions/ForzaFixturesUIView.swift
@@ -0,0 +1,41 @@
+//
+//  ForzaFixturesUIView.swift
+//  ForzaFixtures
+//
+//  Created by ViktorX on 9/30/19.
+//  Copyright © 2019 ViktorX. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+extension UIView {
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: layer.borderColor!)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+}
